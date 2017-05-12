ef18cb163fed26136d43720629fc4c9da7b623b8
Benjamin Schwitter <benjamin.schwitter@gmail.com>
2017-05-12 08:54:34 +0200

    Added typescript definitions (#3)
    * Added typescript definitions
    - we are using the library in our typescript apps but always have to include our own, separate typescript defintion file.
    - now including this into the library itself
    
    * Extended IToken
    - Fixed type for expire_time by making it a number
    - added renew_time of type number

---
+++ predix-uaa-client.d.ts
@@ -0,0 +1,12 @@
+declare module "predix-uaa-client" {
+    export function getToken(url: string, clientId: string, clientSecret: string, refreshToken?: string): Promise<IToken>;
+
+    export function clearCache();
+
+    export interface IToken {
+        access_token: string;
+        refresh_token: string;
+        expire_time: number;
+        renew_time: number;
+    }
+}
